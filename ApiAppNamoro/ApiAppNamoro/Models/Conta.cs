f70441006ea8656cdd6a9d671c9647484152e5f8
Victor Augusto <victor.liger0800@gmail.com>
2023-12-11 17:16:02 -0300

    Versão 0.0.1

---
+++ ApiAppNamoro/ApiAppNamoro/Models/Conta.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiAppNamoro.Models
+{
+    public class Conta
+    {
+        [Key]
+        public int ContaId { get; set; }
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string FotoPerfil { get; set; }
+    }
+}
