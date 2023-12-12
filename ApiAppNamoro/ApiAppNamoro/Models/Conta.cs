773d349ba9bfeb16079db7d933f39c560d18d2f5
Victor Liger <128982763+VicLiger@users.noreply.github.com>
2023-12-11 22:08:06 -0300

    Update Conta.cs (Att 0.0.2)

--- ApiAppNamoro/ApiAppNamoro/Models/Conta.cs
+++ ApiAppNamoro/ApiAppNamoro/Models/Conta.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace ApiAppNamoro.Models
 {
@@ -7,8 +7,16 @@ namespace ApiAppNamoro.Models
         [Key]
         public int ContaId { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "O seu nome é muito grande")]
         public string Nome { get; set; }
+
+        [Required]
+        [Range(1,100, ErrorMessage = "Insira um valor válido entre 1 e 100")]
         public int Idade { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Insira uma imagem válida")]
         public string FotoPerfil { get; set; }
     }
 }
